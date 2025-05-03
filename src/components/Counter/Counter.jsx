1e9785e374671f29e5fd234c32dc01090275c531
nazarufakit <onazik08072005@gmail.com>
2025-05-04 01:43:45 +0300

    inc and decr logic

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./Counter.module.css";
 
 const Counter = () => {
-  const hello = (event) => {
-    console.log(`Hello, nazar!`);
-    console.log(event);
-  };
-
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
   return (
     <div className={s.container}>
-      <h1 className={s.counter}>{1}</h1>
+      <h1 className={s.counter}>{count}</h1>
       <div className={s.buttons}>
-        <button onClick={hello} className={s.btn}>
+        <button onClick={decrement} className={s.btn}>
           minus
         </button>
-        <button className={s.btn}>reset</button>
-        <button className={s.btn}>plus</button>
+        <button onClick={reset} className={s.btn}>
+          reset
+        </button>
+        <button onClick={increment} className={s.btn}>
+          plus
+        </button>
       </div>
     </div>
   );
